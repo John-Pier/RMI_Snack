425b00bffb008db39be9c4cfc3e30b7ee68b077b
John Pier <warhamer10tw@gmail.com>
2021-03-09 00:24:04 +0400

    init

---
+++ client/src/johnpier/ClientMain.java
@@ -0,0 +1,8 @@
+package johnpier;
+
+public class ClientMain {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ server/src/johnpier/ServerMain.java
@@ -0,0 +1,8 @@
+package johnpier;
+
+public class ServerMain {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
